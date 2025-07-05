24ff558209b47c91d3a52632a15d2b2358f9c35f
Muhammed Elabd <muhamedelabd@gmail.com>
2025-07-05 11:27:36 +0700

    Completed step 4 of assignment 4 - Add aesd-assignments package configuration for assignment 4 - Set commit hash to afb9aa3 (assignment-4-part-1 tag) - Configure SSH repository URL - Add installation steps for writer, finder.sh, and finder-test.sh utilities

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = afb9aa3 # this hash for (tag: assignment-4-part-1) Merge remote-tracking branch 'assignments-base/assignment4',
+				   # Used this hash because it contains the modificaitons made for finder-test.sh
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Little3gy.git # Got it from git online repository (using git method with SSH URL, not HTTPS)
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -23,6 +25,12 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/ #writer app cross compiled
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/ # finder.sh location in assignment 3 repository
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/  # finder-test.sh location in assignment 3 repository
+										   # in the assignment instructions it's refering to finder.sh, no such a file
+
 endef
 
 $(eval $(generic-package))
+ 

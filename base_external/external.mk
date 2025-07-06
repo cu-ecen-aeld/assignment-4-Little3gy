b9fa96b27eac299386c449496b05f7de81432162
Muhammed Elabd <muhamedelabd@gmail.com>
2025-07-06 21:36:19 +0700

    Fix base_external/external.mk

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,1 +1,2 @@
-include $(sort $(wildcard $($BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
+# No custom global build steps needed yet
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
